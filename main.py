6982d518fac452245f36eb169654c5b66a86f231
Jsm <jmcsloy@gmail.com>
2021-05-02 23:04:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -13,4 +13,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#testig git
